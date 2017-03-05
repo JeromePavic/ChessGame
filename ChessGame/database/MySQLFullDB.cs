2949ee653d4c8e225eb160d0d31e255252326851
JeromePavic <jeromepavic@gmail.com>
2017-03-05 20:12:23 +0100

    creation of project and first files

---
+++ ChessGame/database/MySQLFullDB.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary2.Entities.Generator;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessGame.entities;
+using ChessGame.json;
+
+namespace ChessGame.database
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class MySQLFullDB : DbContext
+    {
+
+
+        public MySQLFullDB()
+            : base(JsonManager.Instance.ReadFile<ConnectionString>(@"..\..\..\jsonconfig\", @"MysqlConfig.json").ToString())
+        {
+            InitLocalMySQL();
+        }
+
+        public void InitLocalMySQL()
+        {
+            if (this.Database.CreateIfNotExists())
+            {
+            }
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
