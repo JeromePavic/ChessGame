2c72b74f06993968deea1d960f57c667604e4fcd
JeromePavic <jeromepavic@gmail.com>
2017-03-06 23:14:53 +0100

    work on entities and SQLManagers

---
+++ ChessGame/entities/ChessBoard.cs
@@ -0,0 +1,21 @@
+﻿using ChessGame.entities.bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.entities
+{
+    public class ChessBoard : BaseDBEntity
+    {
+        private List<Case> cases;
+
+        public List<Case> Cases
+        {
+            get { return cases; }
+            set { cases = value; }
+        }
+
+    }
+}
